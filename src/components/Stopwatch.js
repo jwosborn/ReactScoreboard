fe1138cd95b0f89148064d6969a2590cbcf3d5cd
kelseymegan92 <kelseymegan92@Kelseys-MacBook-Pro.local>
2019-01-18 15:50:22 -0500

    added Stopwatch Function

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,12 +1,43 @@
 import React, {Component} from 'react';
 
 class Stopwatch extends Component {
+
+state = {
+    isRunning: false,
+    elapsedTime: 0,
+    previousTime: 0
+}
+
+componentDidMount(){
+    this.intervalID = setInterval(() => this.tick(), 100);
+}
+
+
+tick = () => {
+    if (this.state.isRunning){
+        const now = Date.now();
+        this.setState( prevState => ({
+            previousTime: now,
+            elapsedTime: prevState.elapsedTime + (now - this.state.previousTime) ,
+        }));
+    }
+}
+
+handleStopwatch = () => {
+    this.setState( prevState => ({
+        isRunning: !prevState.isRunning
+    }));
+    if (!this.state.isRunning){
+        this.setState ({ previousTime: Date.now() });
+    }
+}
     render (){
+        const seconds = Math.floor(this.state.elapsedTime / 1000);
         return (
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
-                <span className="stopwatch-time">0</span>
-                <button>Start</button> 
+                <span className="stopwatch-time">{seconds}</span>
+                <button onClick={this.handleStopwatch}>{this.state.isRunning ? 'Stop' : 'Start'}</button> 
                 <button>Reset</button>
             </div>
 
