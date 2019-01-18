a8b18c953410b92879b817b89cda6ee3f476d21e
kelseymegan92 <kelseymegan92@Kelseys-MacBook-Pro.local>
2019-01-18 14:36:28 -0500

    initiate repo

---
+++ src/components/App.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Player from './Player';
+import AddPlayerForm from './AddPlayerForm';
+
+
+  
+class App extends Component {
+  state = {
+    players: [
+      {
+        name: "Guil",
+        score: 0,
+        id: 1
+      },
+      {
+        name: "Treasure",
+        score: 0,
+        id: 2
+      },
+      {
+        name: "Ashley",
+        score: 0,
+        id: 3
+      },
+      {
+        name: "James",
+        score: 0,
+        id: 4
+      }
+    ]
+  };
+
+ prevPlayerId = 4;
+
+  handleScoreChange = (index, delta) => {
+    this.setState( prevState => ({
+      score: prevState.players[index].score += delta
+    }));
+  }
+
+  handleRemovePlayer = (id) => {
+    this.setState( prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id)
+      };
+    });
+  }
+
+  handleAddPlayer = (name) => {
+    this.setState( prevState => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name,
+            score: 0,
+            id: this.prevPlayerId += 1
+          }
+        ]
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header 
+          title="Scoreboard" 
+          players={this.state.players} 
+        />
+  
+        {/* Players list */}
+        {this.state.players.map( (player, index) =>
+          <Player 
+            name={player.name}
+            score= { player.score }
+            id={player.id}
+            changeScore={ this.handleScoreChange }
+            key={player.id.toString()} 
+            index={index}
+            removePlayer={this.handleRemovePlayer}           
+          />
+        )}
+
+        <AddPlayerForm addPlayer={this.handleAddPlayer}/>
+      </div>
+    );
+  }
+}
+
+export default App;
